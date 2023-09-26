132bb5b865c786f91638c02c7340a0ab10c088a8
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-09-26 13:04:58 +0200

    fix logix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     Returns:
         list: list of pascal's tringle values
     """
-    if n < 0:
+    if n <= 0:
         return []
     if n == 1:
         return [[1]]
