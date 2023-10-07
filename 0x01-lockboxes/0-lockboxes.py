26583c737184d9e6e307a74e227affd527878a8c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-07 12:12:31 +0200

    add algorithm to solve unlocked boxes buzzle

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module that have lockboxes algorithm
+"""
+from typing import List
+
+
+def canUnlockBoxes(boxes: List[List[int]]) -> bool:
+    """initializer for checkboxes Recursively to
+    determine if all boxes can be unlocked or not
+
+    Args:
+        boxes (List[List[int]]): List of Boxes that have keys to each other
+
+    Returns:
+        bool: True if all boxes can be unlocked, false otherwise
+    """
+    unlocked = [0]
+    return canUnlockBoxesRec(boxes, unlocked)
+
+
+def canUnlockBoxesRec(boxes: List[List[int]], unlocked: List[int],
+                      boxesUnloced: int = 1) -> bool:
+    """_summary_
+
+    Args:
+        boxes (List[List[int]]): List of Boxes that have keys to each other
+        unlocked (List[int]): unlocked boxes by its location
+        boxesUnloced (int, optional): number of unlocked boxes so far.
+        Defaults to 1.
+
+    Returns:
+        bool: True if all boxes can be unlocked, call itself otherwise
+    """
+    if boxesUnloced == len(boxes):
+        return True
+    unlockedLen = len(unlocked)
+    for box in unlocked:
+        for key in boxes[box]:
+            if key not in unlocked:
+                unlocked.append(key)
+                boxesUnloced += 1
+    if (len(unlocked) == unlockedLen):
+        return boxesUnloced == len(boxes)
+    return canUnlockBoxesRec(boxes, unlocked, boxesUnloced)
