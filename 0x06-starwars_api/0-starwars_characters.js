5dde24f3f4821fe3274a1ab053cde3c0d580b62c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 20:20:24 +0200

    add script to get star movie actors recursively

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,41 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const args = process.argv;
 
 if (args.length != 3) {
-	process.exit(1);
+  process.exit(1);
 }
 if (parseInt(args[2]) <= 0) {
-	process.exit(1);
+  process.exit(1);
 }
 
 function getUrl(url) {
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, res, body) => {
-			if (err) {
-				reject(err);
-			}
-			resolve(res);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
+    });
+  });
 }
 
 async function getActors(filmId) {
-	const res = await getUrl(
-		`https://swapi-api.alx-tools.com/api/films/${filmId}`
-	);
-	const resObj = JSON.parse(res.body);
-	if (resObj["characters"] !== undefined) {
-		getActorsRec(resObj["characters"]);
-	}
+  const res = await getUrl(
+    `https://swapi-api.alx-tools.com/api/films/${filmId}`
+  );
+  const resObj = JSON.parse(res.body);
+  if (resObj['characters'] !== undefined) {
+    getActorsRec(resObj['characters']);
+  }
 }
 
 async function getActorsRec(characters) {
-	if (characters.length == 0) return;
-	let current = characters.shift();
-	const res = await getUrl(current);
-	getActorsRec(characters);
-	console.log(JSON.parse(res.body)["name"]);
+  if (characters.length == 0) return;
+  let current = characters.shift();
+  const res = await getUrl(current);
+  getActorsRec(characters);
+  console.log(JSON.parse(res.body)['name']);
 }
 
 getActors(args[2]);
