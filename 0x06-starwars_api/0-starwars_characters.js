6dacc675ecfae4a03704a2e271a6c9c99308e158
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-12 20:34:52 +0200

    fix semistandard format issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,14 +2,14 @@
 const request = require('request');
 const args = process.argv;
 
-if (args.length != 3) {
+if (args.length !== 3) {
   process.exit(1);
 }
 if (parseInt(args[2]) <= 0) {
   process.exit(1);
 }
 
-function getUrl(url) {
+function getUrl (url) {
   return new Promise((resolve, reject) => {
     request.get(url, (err, res, body) => {
       if (err) {
@@ -20,22 +20,22 @@ function getUrl(url) {
   });
 }
 
-async function getActors(filmId) {
+async function getActors (filmId) {
   const res = await getUrl(
     `https://swapi-api.alx-tools.com/api/films/${filmId}`
   );
   const resObj = JSON.parse(res.body);
-  if (resObj['characters'] !== undefined) {
-    getActorsRec(resObj['characters']);
+  if (resObj.characters !== undefined) {
+    getActorsRec(resObj.characters);
   }
 }
 
-async function getActorsRec(characters) {
-  if (characters.length == 0) return;
-  let current = characters.shift();
+async function getActorsRec (characters) {
+  if (characters.length === 0) return;
+  const current = characters.shift();
   const res = await getUrl(current);
   getActorsRec(characters);
-  console.log(JSON.parse(res.body)['name']);
+  console.log(JSON.parse(res.body).name);
 }
 
 getActors(args[2]);
