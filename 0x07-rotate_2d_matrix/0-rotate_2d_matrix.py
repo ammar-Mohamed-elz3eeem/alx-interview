97c52a3b4f825d8f7f5c6fbc7d0664d61a643ef2
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-15 13:23:14 +0200

    rotate matrix correctly by reversing every row

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,9 +5,10 @@
 def rotate_2d_matrix(mat):
     """This function will rotate 2D matrix"""
     for row in range(0, len(mat)):
-        for col in range(0, len(mat[row])):
+        for col in range(len(mat[row]) - 1, 0, -1):
             if col >= row:
                 temp = mat[row][col]
                 mat[row][col] = mat[col][row]
                 mat[col][row] = temp
+        mat[row].reverse()
     return mat
