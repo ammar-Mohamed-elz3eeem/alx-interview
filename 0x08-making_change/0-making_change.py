407f9032de67f59a1d5c3e367c554e5eb56b587f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 14:05:50 +0200

    making change functionality

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Making change module
+"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """ return least number of coins to get to the total
+
+    Args:
+        coins (List[int]): list of coins
+        total (int): total amount needed
+
+    Returns:
+        int: least number of coins to get to the total
+    """
+    count = 0
+    if not coins:
+        return -1
+    if total <= 0:
+        return count
+    for coin in sorted(coins)[::-1]:
+        while total >= coin:
+            total -= coin
+            count += 1
+        if total == 0:
+            return count
+    return -1
+
+
